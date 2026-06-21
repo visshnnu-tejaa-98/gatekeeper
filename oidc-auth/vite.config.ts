70b711a8daa4881d076b8320705c1993c9f059bc
Visshnnu Tejaa <visshnnutejaa@gmail.com>
2026-06-21 19:27:34 +0530

    Update configuration wrt tanstack start

--- oidc-auth/vite.config.ts
+++ oidc-auth/vite.config.ts
@@ -1,4 +1,3 @@
-import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import { devtools } from '@tanstack/devtools-vite'
 
@@ -7,38 +6,18 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-const srcDir = fileURLToPath(new URL('./src', import.meta.url))
-
 const config = defineConfig({
-  resolve: {
-    alias: {
-      '@': srcDir,
-      '#': srcDir,
-    },
-    extensions: ['.mjs', '.js', '.mts', '.ts', '.jsx', '.tsx', '.json'],
-  },
-  plugins: [
-    devtools(),
-    tailwindcss(),
-    tanstackStart({
-      spa: {
-        enabled: true,
-        prerender: { outputPath: '/' },
-      },
-    }),
-    viteReact(),
-  ],
+  resolve: { tsconfigPaths: true },
+  plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact()],
   server: {
     proxy: {
       '/api': {
         target: 'http://localhost:9000',
         changeOrigin: true,
-        secure: false,
       },
       '/o': {
         target: 'http://localhost:9000',
         changeOrigin: true,
-        secure: false,
       },
     },
   },
