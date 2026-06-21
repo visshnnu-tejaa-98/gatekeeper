ed6fa0b5e00387f57199625e66a12659a011308b
Visshnnu Tejaa <visshnnutejaa@gmail.com>
2026-06-21 23:05:43 +0530

    Change config

--- oidc-auth/vite.config.ts
+++ oidc-auth/vite.config.ts
@@ -1,14 +1,15 @@
 import { defineConfig } from 'vite'
 import { devtools } from '@tanstack/devtools-vite'
 
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import { nitro } from 'nitro/vite'
+
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
-  plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact()],
+  plugins: [devtools(), tailwindcss(), tanstackStart(), nitro(), viteReact()],
   server: {
     proxy: {
       '/api': {
